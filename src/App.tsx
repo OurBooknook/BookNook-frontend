8ff1e7ba195b9d50ffebf831c028355c4222d4bc
LeeDahee <dduosy9@gmail.com>
2024-04-03 20:16:07 +0900

    setting: tailwind, eslint, react-query, axios 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className="min-h-screen flex justify-center items-center">
+                <h1 className="text-3xl font-bold text-blue-600">
+                    React + Vite + TypeScript + Tailwind
+                </h1>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
