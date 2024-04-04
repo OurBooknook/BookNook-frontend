255001ea7d5208a79e432737803a8725046caa40
LeeDahee <dduosy9@gmail.com>
2024-04-05 00:41:55 +0900

    feat: 푸터 생성 및 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './layouts/Header'
+import Footer from './layouts/Footer'
 
 function App() {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }
