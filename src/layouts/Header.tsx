c2a28f7d3e03ceeb2810907e1aad2abc5d4f3c7a
LeeDahee <dduosy9@gmail.com>
2024-04-05 00:42:39 +0900

    design: 헤더 90rem으로 변경

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -13,29 +13,31 @@ export default function Header() {
     }
 
     return (
-        <header className="flex flex-row justify-between p-3 bg-primary">
-            <Logo />
-            <div className="flex flex-row justify-between gap-4">
-                <ul className="flex flex-row items-center gap-4 text-lg text-white">
-                    <li>서재</li>
-                    <li>통계</li>
-                </ul>
-                <form className="flex items-center px-4 py-0 rounded-3xl bg-white">
-                    <input
-                        type="search"
-                        className="bg-transparent text-md outline-none"
-                        placeholder="도서를 검색하세요"
-                        onChange={handleChangeSearch}
-                    />
-                    <button
-                        type="submit"
-                        className="text-2xl text-primary"
-                        aria-label="search button"
-                        onClick={handleClickSearch}
-                    >
-                        <IoIosSearch />
-                    </button>
-                </form>
+        <header className="flex justify-center w-full bg-primary">
+            <div className="flex flex-row justify-between p-3 w-[90rem]">
+                <Logo />
+                <div className="flex flex-row justify-between gap-4">
+                    <ul className="flex flex-row items-center gap-4 text-lg text-white">
+                        <li>서재</li>
+                        <li>통계</li>
+                    </ul>
+                    <form className="flex items-center px-4 py-0 rounded-3xl bg-white">
+                        <input
+                            type="search"
+                            className="bg-transparent text-md outline-none"
+                            placeholder="도서를 검색하세요"
+                            onChange={handleChangeSearch}
+                        />
+                        <button
+                            type="submit"
+                            className="text-2xl text-primary"
+                            aria-label="search button"
+                            onClick={handleClickSearch}
+                        >
+                            <IoIosSearch />
+                        </button>
+                    </form>
+                </div>
             </div>
         </header>
     )
