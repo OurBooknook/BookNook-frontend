064585b6dad35662123da3dedf48e22961898e29
LeeDahee <dduosy9@gmail.com>
2024-04-08 02:17:54 +0900

    design: wrapper 간격 조정

--- src/layouts/Wrapper.tsx
+++ src/layouts/Wrapper.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function Wrapper({ children }: { children: React.ReactNode }) {
-    return <div className="self-center w-[90rem] mx-auto">{children}</div>
+    return (
+        <div className="self-center max-w-[90rem] mx-auto mt-12 px-3">
+            {children}
+        </div>
+    )
 }
