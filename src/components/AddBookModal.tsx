cf9128eb04a34958405c5e2fa17a3c6b18186783
LeeDahee <dduosy9@gmail.com>
2024-04-09 01:23:56 +0900

    feat: 서재에 담기 모달 열고 닫기 구현

--- src/components/AddBookModal.tsx
+++ src/components/AddBookModal.tsx
@@ -28,7 +28,11 @@ const bookTypes = [
     },
 ]
 
-export default function AddBookModal() {
+export default function AddBookModal({
+    setIsOpenModal,
+}: {
+    setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>
+}) {
     const [currentType, setCurrentType] = useState('read')
     // const [bookRecord, setBookRecord] = useState({
     //     status: currentType.toUpperCase(),
@@ -44,46 +48,55 @@ export default function AddBookModal() {
         setCurrentType(type)
     }
 
+    const handleCloseModal = () => {
+        setIsOpenModal((prev) => !prev)
+    }
+
     return (
-        <div className="relative flex flex-col p-8 w-[35rem] h-[36rem] bg-primaryVariant rounded-lg">
-            <h1 className="mb-4 text-2xl font-bold">책 담기</h1>
-            <div className="flex justify-between mb-6">
-                {bookTypes.map((bookType) => (
+        <div className="absolute w-full h-full flex justify-center items-center bg-black/60 z-10">
+            <div className="relative flex flex-col p-8 w-[35rem] h-[36rem] bg-primaryVariant rounded-lg">
+                <h1 className="mb-4 text-2xl font-bold">책 담기</h1>
+                <div className="flex justify-between mb-6">
+                    {bookTypes.map((bookType) => (
+                        <button
+                            type="button"
+                            key={bookType.id}
+                            className={`flex flex-col justify-center items-center w-40 py-4 rounded-lg ${bookType.type === currentType ? 'bg-accent' : 'bg-lightGray'}`}
+                            onClick={() => handleClickBookType(bookType.type)}
+                        >
+                            <p className="text-3xl">{bookType.icon}</p>
+                            <h3 className="text-xl font-bold">
+                                {bookType.title}
+                            </h3>
+                            <p className="text-md font-semibold">
+                                {bookType.description}
+                            </p>
+                        </button>
+                    ))}
+                </div>
+                {/* eslint-disable no-nested-ternary */}
+                {currentType === 'read' ? (
+                    <ReadBook />
+                ) : currentType === 'reading' ? (
+                    <ReadingBook />
+                ) : (
+                    <WishBook />
+                )}
+                <div className="absolute bottom-8 flex gap-4 self-end">
                     <button
                         type="button"
-                        key={bookType.id}
-                        className={`flex flex-col justify-center items-center w-40 py-4 rounded-lg ${bookType.type === currentType ? 'bg-accent' : 'bg-lightGray'}`}
-                        onClick={() => handleClickBookType(bookType.type)}
+                        className="text-lg font-bold px-4 py-2 bg-lightGray text-black rounded-md"
+                        onClick={handleCloseModal}
                     >
-                        <p className="text-3xl">{bookType.icon}</p>
-                        <h3 className="text-xl font-bold">{bookType.title}</h3>
-                        <p className="text-md font-semibold">
-                            {bookType.description}
-                        </p>
+                        닫기
                     </button>
-                ))}
-            </div>
-            {/* eslint-disable no-nested-ternary */}
-            {currentType === 'read' ? (
-                <ReadBook />
-            ) : currentType === 'reading' ? (
-                <ReadingBook />
-            ) : (
-                <WishBook />
-            )}
-            <div className="absolute bottom-8 flex gap-4 self-end">
-                <button
-                    type="button"
-                    className="text-lg font-bold px-4 py-2 bg-lightGray text-black rounded-md"
-                >
-                    닫기
-                </button>
-                <button
-                    type="button"
-                    className="text-lg font-bold px-4 py-2 bg-primary text-white rounded-md"
-                >
-                    저장
-                </button>
+                    <button
+                        type="button"
+                        className="text-lg font-bold px-4 py-2 bg-primary text-white rounded-md"
+                    >
+                        저장
+                    </button>
+                </div>
             </div>
         </div>
     )
--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -1,75 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBookmark } from 'react-icons/fa'
 import Wrapper from '../layouts/Wrapper'
+import AddBookModal from '../components/AddBookModal'
+import Header from '../layouts/Header'
+import Footer from '../layouts/Footer'
 
 export default function BookDetail() {
+    const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
     return (
-        <Wrapper>
-            <div className="flex gap-8">
-                {/* NOTE 예시 이미지 */}
-                <img
-                    src="https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788998441012.jpg"
-                    alt="도서 이미지"
-                    className="w-96 h-fit"
-                />
-                <div className="relative w-full">
-                    <div className="flex flex-col gap-3 text-xl">
-                        <h1 className="text-4xl">
-                            도서 제목이 이렇게나 길어지면 어떻게 보여질까요?
-                            한번 확인해보겠습니다
-                        </h1>
-                        <div className="flex gap-4">
-                            <ul className="flex gap-2">
-                                <span>저자: </span>
-                            </ul>
-                            <ul className="flex gap-2">
-                                <span>번역: </span>
-                            </ul>
+        <>
+            {isOpenModal && <AddBookModal setIsOpenModal={setIsOpenModal} />}
+            <Header />
+            <Wrapper>
+                <div className="flex gap-8">
+                    {/* NOTE 예시 이미지 */}
+                    <img
+                        src="https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788998441012.jpg"
+                        alt="도서 이미지"
+                        className="w-96 h-fit"
+                    />
+                    <div className="relative w-full">
+                        <div className="flex flex-col gap-3 text-xl">
+                            <h1 className="text-4xl">
+                                도서 제목이 이렇게나 길어지면 어떻게 보여질까요?
+                                한번 확인해보겠습니다
+                            </h1>
+                            <div className="flex gap-4">
+                                <ul className="flex gap-2">
+                                    <span>저자: </span>
+                                </ul>
+                                <ul className="flex gap-2">
+                                    <span>번역: </span>
+                                </ul>
+                            </div>
+                            <div className="flex gap-4">
+                                <p>출판사</p>
+                                <p>출판 날짜</p>
+                            </div>
+                            <p>
+                                도서 설명이 이렇게나 길어지면 어떻게 보여질까요?
+                                한번 확인해보겠습니다도서 설명이 이렇게나
+                                길어지면 어떻게 보여질까요? 한번
+                                확인해보겠습니다도서 설명이 이렇게나 길어지면
+                                어떻게 보여질까요? 한번 확인해보겠습니다도서
+                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
+                                확인해보겠습니다도서 설명이 이렇게나 길어지면
+                                어떻게 보여질까요? 한번 확인해보겠습니다도서
+                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
+                                확인해보겠습니다도서 설명이 이렇게나 길어지면
+                                어떻게 보여질까요? 한번 확인해보겠습니다도서
+                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
+                                확인해보겠습니다도서 설명이 이렇게나 길어지면
+                                어떻게 보여질까요? 한번 확인해보겠습니다도서
+                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
+                                확인해보겠습니다도서 설명이 이렇게나 길어지면
+                                어떻게 보여질까요? 한번 확인해보겠습니다도서
+                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
+                                확인해보겠습니다도서 설명이 이렇게나 길어지면
+                                어떻게 보여질까요? 한번 확인해보겠습니다도서
+                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
+                                확인해보겠습니다도서 설명이 이렇게나 길어지면
+                                어떻게 보여질까요? 한번 확인해보겠습니다
+                            </p>
                         </div>
-                        <div className="flex gap-4">
-                            <p>출판사</p>
-                            <p>출판 날짜</p>
+                        <div className="absolute bottom-0 flex flex-col gap-2 ">
+                            <p className="text-xl">
+                                담은 사람{' '}
+                                <span className="font-bold text-primary">
+                                    n
+                                </span>
+                                명
+                            </p>
+                            <button
+                                type="button"
+                                className="flex gap-2 items-center px-6 py-3 bg-primary text-white text-xl rounded-md "
+                                onClick={() => setIsOpenModal(true)}
+                            >
+                                <FaBookmark />내 서재에 담기
+                            </button>
                         </div>
-                        <p>
-                            도서 설명이 이렇게나 길어지면 어떻게 보여질까요?
-                            한번 확인해보겠습니다도서 설명이 이렇게나 길어지면
-                            어떻게 보여질까요? 한번 확인해보겠습니다도서 설명이
-                            이렇게나 길어지면 어떻게 보여질까요? 한번
-                            확인해보겠습니다도서 설명이 이렇게나 길어지면 어떻게
-                            보여질까요? 한번 확인해보겠습니다도서 설명이
-                            이렇게나 길어지면 어떻게 보여질까요? 한번
-                            확인해보겠습니다도서 설명이 이렇게나 길어지면 어떻게
-                            보여질까요? 한번 확인해보겠습니다도서 설명이
-                            이렇게나 길어지면 어떻게 보여질까요? 한번
-                            확인해보겠습니다도서 설명이 이렇게나 길어지면 어떻게
-                            보여질까요? 한번 확인해보겠습니다도서 설명이
-                            이렇게나 길어지면 어떻게 보여질까요? 한번
-                            확인해보겠습니다도서 설명이 이렇게나 길어지면 어떻게
-                            보여질까요? 한번 확인해보겠습니다도서 설명이
-                            이렇게나 길어지면 어떻게 보여질까요? 한번
-                            확인해보겠습니다도서 설명이 이렇게나 길어지면 어떻게
-                            보여질까요? 한번 확인해보겠습니다도서 설명이
-                            이렇게나 길어지면 어떻게 보여질까요? 한번
-                            확인해보겠습니다도서 설명이 이렇게나 길어지면 어떻게
-                            보여질까요? 한번 확인해보겠습니다도서 설명이
-                            이렇게나 길어지면 어떻게 보여질까요? 한번
-                            확인해보겠습니다
-                        </p>
-                    </div>
-                    <div className="absolute bottom-0 flex flex-col gap-2 ">
-                        <p className="text-xl">
-                            담은 사람{' '}
-                            <span className="font-bold text-primary">n</span>명
-                        </p>
-                        <button
-                            type="button"
-                            className="flex gap-2 items-center px-6 py-3 bg-primary text-white text-xl rounded-md "
-                        >
-                            <FaBookmark />내 서재에 담기
-                        </button>
                     </div>
                 </div>
-            </div>
-        </Wrapper>
+            </Wrapper>
+            <Footer />
+        </>
     )
 }
