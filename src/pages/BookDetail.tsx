ecf6d7fbbad8d813d7cfad0eebb7d8672b6c6b22
LeeDahee <dduosy9@gmail.com>
2024-04-16 18:07:28 +0900

    feat: 도서 상세페이지 isbn 검색 결과로 변경

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaBookmark } from 'react-icons/fa'
+import { useParams } from 'react-router-dom'
 import Wrapper from '../layouts/Wrapper'
 import AddBookModal from '../components/AddBookModal'
 import Header from '../layouts/Header'
 import Footer from '../layouts/Footer'
+import getSearchBook from '../services/searchBook'
+import { searchDocumentType } from '../types/searchResultType'
+import getFormattedDate from '../utils/getFormattedDate'
 
 export default function BookDetail() {
+    const { isbn } = useParams()
     const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
+    const [searchDocument, setSearchDocument] = useState<searchDocumentType>()
+
+    useEffect(() => {
+        if (isbn !== undefined) {
+            getSearchBook(isbn).then((result) =>
+                setSearchDocument(result.documents[0])
+            )
+        }
+    })
+
     return (
         <>
             {isOpenModal && <AddBookModal setIsOpenModal={setIsOpenModal} />}
@@ -15,53 +30,44 @@ export default function BookDetail() {
                 <div className="flex gap-8">
                     {/* NOTE 예시 이미지 */}
                     <img
-                        src="https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9788998441012.jpg"
-                        alt="도서 이미지"
+                        src={searchDocument?.thumbnail}
+                        alt={`${searchDocument?.title} 도서 이미지`}
                         className="w-96 h-fit"
                     />
                     <div className="flex flex-col gap-12 w-full">
                         <div className="flex flex-col gap-3 text-xl">
                             <h1 className="text-4xl">
-                                도서 제목이 이렇게나 길어지면 어떻게 보여질까요?
-                                한번 확인해보겠습니다
+                                {searchDocument?.title}
                             </h1>
                             <div className="flex gap-4">
                                 <ul className="flex gap-2">
                                     <span>저자: </span>
+                                    {/* eslint-disable react/no-array-index-key */}
+                                    {searchDocument?.authors.map(
+                                        (author, index) => (
+                                            <li key={index}>{author}</li>
+                                        )
+                                    )}
                                 </ul>
                                 <ul className="flex gap-2">
                                     <span>번역: </span>
+                                    {/* eslint-disable react/no-array-index-key */}
+                                    {searchDocument?.translators.map(
+                                        (translator, index) => (
+                                            <li key={index}>{translator}</li>
+                                        )
+                                    )}
                                 </ul>
                             </div>
                             <div className="flex gap-4">
-                                <p>출판사</p>
-                                <p>출판 날짜</p>
+                                <p>{searchDocument?.publisher}</p>
+                                <p>
+                                    {getFormattedDate(
+                                        searchDocument?.datetime ?? ''
+                                    )}
+                                </p>
                             </div>
-                            <p>
-                                도서 설명이 이렇게나 길어지면 어떻게 보여질까요?
-                                한번 확인해보겠습니다도서 설명이 이렇게나
-                                길어지면 어떻게 보여질까요? 한번
-                                확인해보겠습니다도서 설명이 이렇게나 길어지면
-                                어떻게 보여질까요? 한번 확인해보겠습니다도서
-                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
-                                확인해보겠습니다도서 설명이 이렇게나 길어지면
-                                어떻게 보여질까요? 한번 확인해보겠습니다도서
-                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
-                                확인해보겠습니다도서 설명이 이렇게나 길어지면
-                                어떻게 보여질까요? 한번 확인해보겠습니다도서
-                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
-                                확인해보겠습니다도서 설명이 이렇게나 길어지면
-                                어떻게 보여질까요? 한번 확인해보겠습니다도서
-                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
-                                확인해보겠습니다도서 설명이 이렇게나 길어지면
-                                어떻게 보여질까요? 한번 확인해보겠습니다도서
-                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
-                                확인해보겠습니다도서 설명이 이렇게나 길어지면
-                                어떻게 보여질까요? 한번 확인해보겠습니다도서
-                                설명이 이렇게나 길어지면 어떻게 보여질까요? 한번
-                                확인해보겠습니다도서 설명이 이렇게나 길어지면
-                                어떻게 보여질까요? 한번 확인해보겠습니다
-                            </p>
+                            <p>{searchDocument?.contents}</p>
                         </div>
                         <div className="flex flex-col gap-2 w-fit">
                             <p className="text-xl">
