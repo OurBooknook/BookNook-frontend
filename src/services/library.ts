83d8fa72c662fb26248a3faccaf9d6a84c723990
LeeDahee <dduosy9@gmail.com>
2024-04-18 19:26:05 +0900

    feat: 서재 상세 정보 조회 함수 작성, export 변경

--- src/services/library.ts
+++ src/services/library.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Status } from '../types/bookType'
 import responseType from '../types/responseType'
+import { LibraryDetailType, RecordType } from '../types/libraryType'
 
 export interface LibraryType {
     totalPages: number
@@ -8,7 +9,7 @@ export interface LibraryType {
     isbnList: string[]
 }
 
-const getLibrary = async (
+export const getLibrary = async (
     status: Status,
     page: number
 ): Promise<LibraryType> => {
@@ -23,4 +24,37 @@ const getLibrary = async (
     return response.data.results
 }
 
-export default getLibrary
+export const getLibraryDetail = async (
+    isbn: string
+): Promise<LibraryDetailType> => {
+    const response = await axios.get<
+        responseType<{
+            isbn: string
+            status: Status
+            rate: number | null
+            startDate: string | null
+            finishDate: string | null
+            page: number | null
+            expectation: string | null
+            recordList: RecordType[]
+        }>
+    >(`${process.env.REACT_APP_API}/api/library`, {
+        params: {
+            isbn,
+        },
+    })
+    const responseData = response.data.results
+
+    const libraryDetail: LibraryDetailType = {
+        isbn: responseData.isbn,
+        status: responseData.status,
+        rate: responseData.rate,
+        startDate: responseData.startDate,
+        endDate: responseData.finishDate,
+        page: responseData.page,
+        expectation: responseData.expectation,
+        recordList: responseData.recordList,
+    }
+
+    return libraryDetail
+}
