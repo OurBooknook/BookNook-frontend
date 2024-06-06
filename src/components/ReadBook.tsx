cc0e580f8f98116e7735edb2ee550bf2932280a8
LeeDahee <dduosy9@gmail.com>
2024-06-06 14:33:50 +0900

    refactor: 읽은 책 별점 렌더링 리팩토링

--- src/components/ReadBook.tsx
+++ src/components/ReadBook.tsx
@@ -9,29 +9,20 @@ export default function ReadBook({
     readInfo: ReadInfoType
     setReadInfo: React.Dispatch<React.SetStateAction<ReadInfoType>>
 }) {
-    // FIXME 효율적이지 않은 코드 같음. 하드코딩 아닌 방식으로 구현
-    const [starScore, setStarScore] = useState<number>()
-    const [starArr, setStarArr] = useState<number[]>([1, 1, 1, 0, 0])
-    const handleClickStarScore = (value: number) => {
-        switch (value) {
-            case 0:
-                setStarArr([1, 0, 0, 0, 0])
-                break
-            case 1:
-                setStarArr([1, 1, 0, 0, 0])
-                break
-            case 2:
-                setStarArr([1, 1, 1, 0, 0])
-                break
-            case 3:
-                setStarArr([1, 1, 1, 1, 0])
-                break
-            case 4:
-                setStarArr([1, 1, 1, 1, 1])
-                break
-            default:
-                setStarArr([1, 1, 1, 1, 1])
+    const getStarArr = (starScore: number): number[] => {
+        const arr = new Array(5).fill(0)
+        for (let i = 0; i < starScore; i += 1) {
+            arr[i] = 1
         }
+        return arr
+    }
+    // FIXME 효율적이지 않은 코드 같음. 하드코딩 아닌 방식으로 구현
+    const [starArr, setStarArr] = useState<number[]>(
+        getStarArr(readInfo.starScore ?? 0)
+    )
+    const handleClickStarScore = (index: number) => {
+        const updatedStarArr = starArr.map((star, i) => (i <= index ? 1 : 0))
+        setStarArr(updatedStarArr)
     }
 
     useEffect(() => {
@@ -40,10 +31,6 @@ export default function ReadBook({
             count += num === 1 ? 1 : 0
         })
 
-        console.log(count)
-        console.log(starScore)
-
-        setStarScore(count)
         setReadInfo((prevValue) => ({ ...prevValue, starScore: count }))
     }, [starArr])
 
