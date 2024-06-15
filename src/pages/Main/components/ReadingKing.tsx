ab7c2e2905dc07ee41198acf0773064b65958cc5
LeeDahee <dduosy9@gmail.com>
2024-06-15 21:59:21 +0900

    design: 독서왕 컴포넌트 css

--- src/pages/Main/components/ReadingKing.tsx
+++ src/pages/Main/components/ReadingKing.tsx
@@ -1,4 +1,4 @@
-import { Chip } from '@mui/material'
+import { Avatar, Chip } from '@mui/material'
 import React from 'react'
 
 interface readingKingType {
@@ -21,13 +21,29 @@ export default function ReadingKing({
     }
 
     return (
-        <div>
-            <div>
-                <img src={profileImg} alt="독서왕 프로필 이미지" />
-                <span>{medal[rank]}</span>
+        <div className="flex flex-col justify-center items-center py-6 bg-primaryVariant rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+            <div className="relative mb-2">
+                <Avatar
+                    alt={`${nickname}의 프로필 사진`}
+                    src={profileImg}
+                    sx={{ width: 80, height: 80 }}
+                />
+                <span className="absolute -top-2 -right-2 text-4xl">
+                    {medal[rank]}
+                </span>
             </div>
-            <h3>{nickname}</h3>
-            <Chip label={`📚 ${books}권`} color="success" size="small" />
+            <h3 className="text-lg font-bold mb-1">{nickname}</h3>
+            <Chip
+                label={`📚 ${books}권`}
+                color="success"
+                size="small"
+                sx={{
+                    fontSize: 14,
+                    paddingX: 0.5,
+                    paddingY: 1,
+                    fontWeight: 'bold',
+                }}
+            />
         </div>
     )
 }
