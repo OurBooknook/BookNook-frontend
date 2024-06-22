0dbbac2a66dbf244affd4798ea8b6b8e5414c45e
LeeDahee <dduosy9@gmail.com>
2024-06-22 18:48:27 +0900

    feat: 통계페이지 생성, 라우터 연결

---
+++ src/pages/Statistics/Statistics.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material'
+import Header from '../../components/Header'
+import Wrapper from '../../components/Wrapper'
+import Footer from '../../components/Footer'
+
+export default function Statistics() {
+    const [year, setYear] = useState<string>('2024')
+    const yearArr: string[] = ['2024', '2023', '2022', '2021', '2020']
+
+    const handleSelectYear = (e: SelectChangeEvent) => {
+        setYear(e.target.value as string)
+    }
+    return (
+        <>
+            <Header />
+            <Wrapper>
+                <div className="flex justify-between">
+                    <h1>통계</h1>
+                    <Select
+                        value={year}
+                        label="year"
+                        onChange={handleSelectYear}
+                    >
+                        {yearArr.map((value) => (
+                            <MenuItem value={value}>{value}</MenuItem>
+                        ))}
+                    </Select>
+                </div>
+            </Wrapper>
+            <Footer />
+        </>
+    )
+}
